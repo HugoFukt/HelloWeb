c80d99910512c4b0a6f500ad826b8267a2a019e6
YuXinyang <1048612051@qq.com>
2018-09-29 10:50:59 +0800

    change

--- src/main/java/com/yiibai/springmvc/HelloController.java
+++ src/main/java/com/yiibai/springmvc/HelloController.java
@@ -3,20 +3,23 @@ package com.yiibai.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.ModelMap;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController{
 
-	@RequestMapping(value = "/user/createUser",method = RequestMethod.GET)
+
+	@RequestMapping(method = RequestMethod.GET)
    public String printHello(ModelMap model) {
       model.addAttribute("message", "Hello World!");
 
       return "hello";
    }
-//	@RequestMapping(value = "/user/createUser", method = RequestMethod.GET)
-//	public String getString(ModelMap model) {
-//		return "Hello World!";
-//	}
+	@ResponseBody
+	@RequestMapping(value = "/user/createUser", method = RequestMethod.GET)
+	public String getString(ModelMap model) {
+		return "Hello World!";
+	}
 }
--- src/test/java/com/yiibai/springmvc/test/testcase/TestCaseLaunchApiController.java
+++ src/test/java/com/yiibai/springmvc/test/testcase/TestCaseLaunchApiController.java
@@ -6,9 +6,11 @@ import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import com.yiibai.springmvc.test.URLCon4;
+
 //import com.yiibai.springmvc.test.URLCon;
 //import com.yiibai.springmvc.test.URLCon2;
-import com.yiibai.springmvc.test.URLCon3;
+//import com.yiibai.springmvc.test.URLCon3;
 
 
 
@@ -19,13 +21,14 @@ public class TestCaseLaunchApiController {
     @Test(groups = {"BaseCase"})
     public void getSecond() throws IOException{
     	String url = "http://192.168.101.34:8081/HelloWeb/hello/user/createUser";
-    	//String params = "";
+    	String params = "";
     	//String result = URLCon.getConnection(url, params,"GET");
     	//String result = URLCon2.doHttpRequest(url);
-    	String result = URLCon3.isValid(url);
+    	//String result = URLCon3.isValid(url);
+    	String result = URLCon4.sendGet(url, params);
         Reporter.log("リクエストURL: "+ url);
         Reporter.log("結果: "+ result);
-        Assert.assertEquals(result,"true");
+        Assert.assertEquals(result,"Hello World!");
     }
     
 }
